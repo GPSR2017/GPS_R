b99d7ebb84bafa21b4793938124618dda97c89b4
Peter <peter.matheny@gmail.com>
2017-02-13 17:06:28 -0600

    Initial progress on the function prototypes
    Added structures for the measurement, GPR data and video frame.  Also added functions that grab the GPS coordinates from a video frame, one to combine a videoFrame structure and a GPREntry structure and finally a function that is intended to read in video.

--- prototypes.cpp
+++ prototypes.cpp
@@ -1,3 +1,36 @@
-/*
+/**
+
 File containing the function prototypes.
-*/
+
+**/
+#include <string>
+#include <iostream>
+struct measurement{
+  float time;
+  float lat;
+  float lon;
+  float GPR_depth;
+};
+
+struct videoFrame{
+  float time;
+  float lat;
+  float lon;
+};
+
+struct GPREntry{
+  float time;
+  float depth;
+};
+
+videoFrame video2GPS(int frame_number);
+
+float* inputVideo(std::string videoFilename);
+
+measurement correlateTimes(videoFrame vid_frame, GPREntry GPR_data);
+
+int main()
+{
+  std::cout<<"The prototypes have compiled.\n";
+}
+
