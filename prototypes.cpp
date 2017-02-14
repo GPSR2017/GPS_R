e977c82c5520f9f97e265a2123b71474838b1200
ColtonBaker <cbaker7@memphis.edu>
2017-02-14 14:02:52 -0600

    Formatting changes proposed by Dr. Taborda.

--- prototypes.cpp
+++ prototypes.cpp
@@ -5,31 +5,30 @@ File containing the function prototypes.
 **/
 #include <string>
 #include <iostream>
-struct measurement{
+struct measurement {
     float time;
     float lat;
     float lon;
     float GPR_depth;
 };
 
-struct videoFrame{
+struct videoFrame {
     float time;
     float lat;
     float lon;
 };
 
-struct GPREntry{
+struct GPREntry {
     float time;
     float depth;
 };
 
-videoFrame video2GPS(int frame_number);
+videoFrame video2GPS (int frame_number);
 
-float* inputVideo(std::string videoFilename);
+float* inputVideo (std::string videoFilename);
 
-measurement correlateTimes(videoFrame vid_frame, GPREntry GPR_data);
+measurement correlateTimes (videoFrame vid_frame, GPREntry GPR_data);
 
-int main()
-{
+int main() {
     std::cout<<"The prototypes have compiled.\n";
 }
