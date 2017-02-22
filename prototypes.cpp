9b628f462e82703ae6001e187977eda987c73251
ColtonBaker <cbaker7@memphis.edu>
2017-02-22 15:46:44 -0600

    Added prototypes for Colton's functions.

--- prototypes.cpp
+++ prototypes.cpp
@@ -31,6 +31,20 @@ float* inputVideo (std::string videoFilename);
 
 measurement correlateTimes (videoFrame vid_frame, GPREntry GPR_data);
 
+// A function that will read and convert the time from the .csv GPR data file
+measurement readGPRTime (float GPRTime );
+
+// A function that will convert the time found in the GPR file into a real time
+//corresponding to the video file
+measurement convertGPRTime ( measurement time);
+
+// A function that will read the GPR depth in the .csv GPR data file
+measurement readGPRDepth ( measurement GPR_Depth );
+
+// Functions that will read latitude and longitude from GPR file
+measurement readGPRLat ( measurement lat );
+measurement readGPRLon ( measurement lon);
+
 int main() {
     std::cout<<"The prototypes have compiled.\n";
 }
