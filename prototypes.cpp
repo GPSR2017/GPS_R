d644d703e75a62ac4905ff77800142be94bfc0a6
cbaker415 <cbaker7@memphis.edu>
2017-02-23 11:33:23 -0600

    Switched prototypes comments to Doxygen style.

--- prototypes.cpp
+++ prototypes.cpp
@@ -31,17 +31,17 @@ float* inputVideo (std::string videoFilename);
 
 measurement correlateTimes (videoFrame vid_frame, GPREntry GPR_data);
 
-// A function that will read and convert the time from the .csv GPR data file
+/// A function that will read and convert the time from the .csv GPR data file
 measurement readGPRTime (float GPRTime );
 
-// A function that will convert the time found in the GPR file into a real time
-//corresponding to the video file
+/// A function that will convert the time found in the GPR file into a real time
+/// corresponding to the video file
 measurement convertGPRTime ( measurement time);
 
-// A function that will read the GPR depth in the .csv GPR data file
+/// A function that will read the GPR depth in the .csv GPR data file
 measurement readGPRDepth ( measurement GPR_Depth );
 
-// Functions that will read latitude and longitude from GPR file
+/// Functions that will read latitude and longitude from GPR file
 measurement readGPRLat ( measurement lat );
 measurement readGPRLon ( measurement lon);
 
